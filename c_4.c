cc24539b158cd3e5077fbbb92047056c968f130f
mgttttt <92685359+mgttttt@users.noreply.github.com>
2022-03-01 15:59:34 +0500

    4

---
+++ c_4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#pragma warning(disable : 4996)
+int main() {
+    double n; double sum = 0;
+    double h = 0.02;
+    double old_I = 0;
+
+    printf("Enter n: ");
+    scanf("%lf", &n);
+
+    do {
+        old_I = sum;
+        sum = 0; h /= 2.0;
+        for (int i = 0; i < 2 / h; i++)
+        {
+            if (i * h <= 1)
+                sum += 8 * pow(i * h + h / 2.0, 3) * cos(i * h + h / 2.0);
+            else sum += log(1 + sqrt(i * h + h / 2.0)) - cos(i * h + h / 2.0);
+        }
+        sum *= h;
+    } while (fabs(sum - old_I) / 3.0 >= n);
+    printf("%lf", sum);
+
+    return 0;
+}
