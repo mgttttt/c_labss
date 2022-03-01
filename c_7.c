a0e10dd858f51644fca730d84d86f8a79318a2b3
mgttttt <92685359+mgttttt@users.noreply.github.com>
2022-03-01 16:02:21 +0500

    7

---
+++ c_7.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+void fill(int m, int n, int array[][n])
+{
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            array[i][j] = rand() % 200 + 1;
+}
+
+void out(int m, int n, int arr[][n])
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            printf("%4d", arr[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+double findNorm(int m, int n, int arr[][n])
+{
+    double result = 0;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            result += pow(arr[i][j], 2);
+    return sqrt(result);
+}
+
+int main() {
+    srand(time(NULL));
+    int n, m;
+    printf("Enter count of rows: ");
+    scanf("%d", &m);
+    printf("Enter count of columns: ");
+    scanf("%d", &n);
+    int array[m][n];
+    fill(m, n, array);
+    out(m, n, array);
+    printf("%lf", findNorm(m, n, array));
+    return 0;
+}
