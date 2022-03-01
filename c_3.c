c111eb70abd39f231402620551970662122de5ef
mgttttt <92685359+mgttttt@users.noreply.github.com>
2022-03-01 15:57:22 +0500

    Create c_3.c

---
+++ c_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#pragma warning(disable : 4996)
+int main() {
+    float h;
+    float x = 0;
+    printf("Enter h: ");
+    scanf("%f", &h); 
+
+    printf("x\t\tf(x)\n");
+    printf("--------------------\n");
+
+    do
+    {
+        if (x <= 1)
+            printf("%f    %f\n", x, 8 * pow(x, 3) * cos(x));
+        else
+            printf("%f    %f\n", x, log(1 + sqrt(x)) - cos(x));
+        x += h;
+
+    } while (x <= 2.000001);
+
+    return 0;
+}
