8141ec87cb2b7185fc9f1757f46623812b5b4068
mgttttt <92685359+mgttttt@users.noreply.github.com>
2022-03-02 16:24:34 +0500

    Update c_3.c

--- c_3.c
+++ c_3.c
@@ -5,12 +5,12 @@ int main() {
     float h;
     float x = 0;
     printf("Enter h: ");
-    scanf("%f", &h); 
+    scanf("%f", &h);
 
     printf("x\t\tf(x)\n");
     printf("--------------------\n");
 
-    do
+    /*do
     {
         if (x <= 1)
             printf("%f    %f\n", x, 8 * pow(x, 3) * cos(x));
@@ -18,7 +18,16 @@ int main() {
             printf("%f    %f\n", x, log(1 + sqrt(x)) - cos(x));
         x += h;
 
-    } while (x <= 2.000001);
+    } while (x <= 2.000001);*/
+    
+    for (int i = 0; i <= 2 / h; i++)
+    {
+        if (x <= 1)
+            printf("%f    %f\n", x, 8 * pow(x, 3) * cos(x));
+        else
+            printf("%f    %f\n", x, log(1 + sqrt(x)) - cos(x));
+        x += h;
+    }
 
     return 0;
 }
