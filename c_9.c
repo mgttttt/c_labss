ac3e91731a1ebdd98f334f095138c111ce11f55c
mgttttt <92685359+mgttttt@users.noreply.github.com>
2022-03-13 16:01:22 +0500

    9

--- c_9.c
+++ c_9.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-int is_pallindrom(char * str)
+#pragma warning (disable:4996)
+int is_pallindrom(char* str)
 {
-  printf("%s\n", str);
-  int len = strlen(str);
-  for(int i = 0; i < len / 2; i++)
-    if (str[i] != str[len - 1 - i]) return 0;
-  printf("%s\n", str);
-  return len;
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++)
+        if (str[i] != str[len - 1 - i]) return 0;
+    return len;
 }
 
 int main() {
-  int temp;
-  char * result = malloc(sizeof(char) * 100);
-  char * str = malloc(sizeof(char) * 100);
-  printf("%s", "Enter string: ");
-  fgets(str, 100, stdin);
-  char delim[] = ", ";
-  char *p = strtok(str, delim);
-  temp = is_pallindrom(p);
-  if (temp && temp > strlen(result))
-    result = p;
-  while((p = strtok(NULL, delim)) != NULL)
-  {
-    printf("%s\n", p);
+    int temp; int max_sz = 0;
+    char* result = (char *)malloc(sizeof(char) * 100);
+    char* str = (char *)malloc(sizeof(char) * 100);
+    printf("%s", "Enter string: ");
+    fgets(str, 100, stdin);
+    char delim[] = ", \n";
+    char* p = strtok(str, delim);
     temp = is_pallindrom(p);
-    if (temp && temp > strlen(result))
-      result = p;
-  }
-  if (strlen(result) == 0) printf("There are no pallindroms\n");
-  else printf("%s\n", result);
-  return 0;
+    if (temp && temp > max_sz)
+    {
+        max_sz = temp;
+        result = p;
+    }
+    while ((p = strtok(NULL, delim)) != NULL)
+    {
+        temp = is_pallindrom(p);
+        if (temp && temp > strlen(result))
+            result = p;
+    }
+    if (max_sz == 0) printf("There are no pallindroms\n");
+    else
+        printf("%s\n", result);
+    return 0;
 }
