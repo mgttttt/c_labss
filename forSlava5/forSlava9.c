cdc1569d9df67e4738c24b6b8cfb080370af9057
mgttttt <92685359+mgttttt@users.noreply.github.com>
2022-05-13 21:05:33 +0500

    !!!!!!!!!!!!!!!!!!

--- forSlava5/forSlava9.c
+++ forSlava5/forSlava9.c
@@ -5,40 +5,41 @@
 void get_frequency(char * str, char * freq, int n)
 {
   for (int i = 0; i < n; i++)
-    freq[str[i]]++;
+    freq[(int)str[i]]++;
 }
 
 int sum_of_freq(char * str, char * freq, int n)
 {
   int sum = 0;
   for (int i = 0; i < n; i++)
-    sum += freq[str[i]];
+    sum += freq[(int)str[i]];
   return sum;
 }
 
 int get_max_freq(char * str, char * freq, int n)
 {
+  char * delim = " ,";
   int max_freq = 0;
   char * p = strtok(str, delim);
-  int freq = sum_of_freq(p, freq, strlen(p));
-  if (freq > max_freq)
-    max_freq = freq;
-  while((p = strtok(str, NULL)) != NULL)
+  int frequency = sum_of_freq(p, freq, strlen(p));
+  if (frequency > max_freq)
+    max_freq = frequency;
+  while((p = strtok(NULL, delim)) != NULL)
   {
-    int freq = sum_of_freq(p, freq, strlen(p));
-    if (freq > max_freq)
-      max_freq = freq;
+    frequency = sum_of_freq(p, freq, strlen(p));
+    if (frequency > max_freq)
+      max_freq = frequency;
   }
   return max_freq;
 }
 
 int main ()
 {
-  char delim = " ,";
+  char * delim = " ,";
   char * str = (char *)malloc(sizeof(char) * 256);
   char * freq = (char *)calloc(256, sizeof(char));
   char * str_cpy = (char *)malloc(sizeof(char) * 256);
-  fgets(stdin, 255, str);
+  fgets(str, 255, stdin);
   strcpy(str_cpy, str);
   int len = strlen(str);
   get_frequency(str, freq, len);
@@ -46,10 +47,11 @@ int main ()
   char * p = strtok(str_cpy, delim);
   if (sum_of_freq(p, freq, strlen(p)) != max_freq)
     printf("%s ", p);
-  while((p = strtok(str_cpy, NULL)) != NULL)
+  while((p = strtok(NULL, delim)) != NULL)
   {
     if (sum_of_freq(p, freq, strlen(p)) != max_freq)
       printf("%s ", p);
   }
+  printf("\n");
   return 0;
 }
