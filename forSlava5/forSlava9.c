45b21a9d9250d4ab2c356fb6ead25b907fc8268d
mgttttt <92685359+mgttttt@users.noreply.github.com>
2022-05-18 18:23:00 +0500

    исправлена

--- forSlava5/forSlava9.c
+++ forSlava5/forSlava9.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 //частотность строки
-//заводим массив на 256 символов - каждый элемент это количество раз 
-//которое встретился элемент в строке  
+//заводим массив на 256 символов - каждый элемент это количество раз
+//которое встретился элемент в строке
 void get_frequency(char * str, char * freq, int n)
 {
   for (int i = 0; i < n; i++)
@@ -21,21 +21,23 @@ int sum_of_freq(char * str, char * freq, int n)
 }
 
 //ищем слово с максимальной частотностью
-//пробел и запятая это разделители 
+//пробел и запятая это разделители
 //с помощью функции strtok сплитим строку по заданным разделителям
 //после каждого вызова функции strtok получаем слово, ищем сумму частотностей
 //возвращается максимальная найденная частотность
 int get_max_freq(char * str, char * freq, int n)
 {
-  char * delim = " ,";
+  char * delim = " ,\n";
   int max_freq = 0;
   char * p = strtok(str, delim);
   int frequency = sum_of_freq(p, freq, strlen(p));
+  printf("frequency of word \"%s\":%d\n", p, frequency);
   if (frequency > max_freq)
     max_freq = frequency;
   while((p = strtok(NULL, delim)) != NULL)
   {
     frequency = sum_of_freq(p, freq, strlen(p));
+    printf("frequency of word \"%s\":%d\n", p, frequency);
     if (frequency > max_freq)
       max_freq = frequency;
   }
@@ -44,7 +46,7 @@ int get_max_freq(char * str, char * freq, int n)
 
 int main ()
 {
-  char * delim = " ,";
+  char * delim = " ,\n";
   char * str = (char *)malloc(sizeof(char) * 256);
   char * freq = (char *)calloc(256, sizeof(char));//тут выделяем с помощью calloc чтобы сразу занулить все элементы
   char * str_cpy = (char *)malloc(sizeof(char) * 256);
@@ -54,7 +56,7 @@ int main ()
   get_frequency(str, freq, len); //считаем частотность
   int max_freq = get_max_freq(str, freq, len); //находим максимальную частотность
   char * p = strtok(str_cpy, delim); //опять сплитим по пробелам и запятым
-  //если частотность слова не совпадает с максимальной, то выводим слово 
+  //если частотность слова не совпадает с максимальной, то выводим слово
   //иначе пропускаем слово, таким образом удаляем слова с максимальной частотностью
   if (sum_of_freq(p, freq, strlen(p)) != max_freq)
     printf("%s ", p);
