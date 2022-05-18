8706de3e7e78c407cba575ef25a06f072177050e
mgttttt <92685359+mgttttt@users.noreply.github.com>
2022-05-18 17:58:39 +0500

    !!!!!!!!!!!

--- forSlava5/forSlava5.c
+++ forSlava5/forSlava5.c
@@ -2,6 +2,7 @@
 int countOfDigits(int n)
 {
     //пока н не равно 0 отбрасываем последнюю цифру и к count_of_dg прибавляем 1
+    if (!n) return 1;
     int count_of_dg = 0;
     while (n)
     {
@@ -13,9 +14,12 @@ int countOfDigits(int n)
 
 int rec_countOfDigits(int n)
 {
+    if (n / 10)
+        return 1 + rec_countOfDigits(n / 10);
+    else
+        return 1;
     //если n не равно нулю вызываем функцию и отбрасываем последнюю цифру n,
     //при этом к возращаемому результату прибавляем 1, иначе завершаем рекурсию
-    return n ? rec_countOfDigits(n / 10) + 1 : 0;
 }
 
 int main() {
@@ -25,17 +29,8 @@ int main() {
     scanf("%d", &number);
     //вызываем обычную функцию и рекурсивную в том случае если н не 0
     //иначе просто выводим 1 без вызова функций
-    if (n)
-    {
-        printf("Count of digits:\n");
-        printf("Cycle: %d\n", countOfDigits(number));
-        printf("Recursion: %d\n", rec_countOfDigits(number));
-    }
-    else
-    {
-        printf("Count of digits:\n");
-        printf("Cycle: %d\n", 1);
-        printf("Recursion: %d\n", 1);
-    }
+    printf("Count of digits:\n");
+    printf("Cycle: %d\n", countOfDigits(number));
+    printf("Recursion: %d\n", rec_countOfDigits(number));
     return 0;
 }
