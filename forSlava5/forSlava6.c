b638cd1d88a6e2b1c290245d9a311adb0e09dc5c
mgttttt <92685359+mgttttt@users.noreply.github.com>
2022-05-18 18:32:19 +0500

    исправлена

--- forSlava5/forSlava6.c
+++ forSlava5/forSlava6.c
@@ -17,27 +17,57 @@ void out(int arr[], int n)
   printf("\n");
 }
 
-//под двойным циклом перебираем все возможные пары элементов 
+//под двойным циклом перебираем все возможные пары элементов
 //если элементы совпадаю инкрементируем count
 //возвращаем количество повторяющихся элементов
 int find_count(int a[], int b[], int n1, int n2)
 {
-  int count = 0;
+  int count = 0, flag = 0;
   for (int i = 0; i < n1; i++)
+  {
+    flag = 0;
     for (int j = 0; j < n2; j++)
-      if (a[i] == b[j])
-        count++;
+    {
+        for (int l = 0; l < i; l++)
+          if (a[i] == a[l])
+          {
+            flag = 1;
+            break;
+          }
+        if (flag) break;
+        if (a[i] == b[j])
+        {
+          count++;
+          break;
+        }
+    }
+  }
   return count;
 }
 
 //снова имем совпадения, но сейчас уже записываем элементы в массив rep
 void find_rep(int rep[], int a[], int b[], int n1, int n2)
 {
-  int count = 0;
+  int count = 0, flag = 0;
   for (int i = 0; i < n1; i++)
+  {
+    flag = 0;
     for (int j = 0; j < n2; j++)
+    {
+      for (int l = 0; l < i; l++)
+        if (a[i] == a[l])
+        {
+          flag = 1;
+          break;
+        }
+      if (flag) break;
       if (a[i] == b[j])
+      {
         rep[count++] = a[i];
+        break;
+      }
+    }
+  }
 }
 
 int main()
@@ -55,7 +85,7 @@ int main()
   //вывод массивов
   out(a, n1);
   out(b, n2);
-  //ищем количество 
+  //ищем количество
   int count = find_count(a, b, n1, n2);
   //если count = 0 выводим сообщение, что повторяющихся элементов нет
   //иначе заполняем массив rep и выводим его
@@ -69,3 +99,4 @@ int main()
   }
   return 0;
 }
+
